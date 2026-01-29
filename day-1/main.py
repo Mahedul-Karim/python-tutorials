e710a8b61be359ce3cdd9e27a62466d3396ecb1d
Mahedul-Karim <karimrupo21@gmail.com>
2026-01-29 16:44:02 +0600

    input function, len function and variables

--- day-1/main.py
+++ day-1/main.py
@@ -1,7 +1,26 @@
-#Multiline string
+###  Multiline string
 
-print("Hello Word\nwith python")
+# print("Hello Word\nwith python")
 
-#string concatination
+### string concatination
 
-print("Hello"+" "+"World")
+# print("Hello"+" "+"World")
+
+###Input function
+
+# input("Put user name")
+# print("Welcome",input("Enter your user name"))
+
+### variables
+
+name = "John Doe"
+print(name)
+
+inputted_name = input("Type your name")
+print(inputted_name)
+
+
+### len function (calculate a strings length)
+
+length_of_name = len(inputted_name)
+print(length_of_name)
