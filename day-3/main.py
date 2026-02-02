a84585b1b5ed36a8f251dca850b763728747ec87
Mahedul-Karim <karimrupo21@gmail.com>
2026-02-02 16:41:34 +0600

    logical not operator

--- day-3/main.py
+++ day-3/main.py
@@ -6,4 +6,14 @@ if height == 120:
     print(f"Your height is {height}cm")
 else:
     print("Your height is less then 120cm")
-    
+
+
+number = input('Write a number to check if it is even')
+is_even_number =not int(number) % 2 ## not is equivalent to js !
+
+
+if is_even_number:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
+
