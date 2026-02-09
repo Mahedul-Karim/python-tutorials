b96e8c7a7c87bed56e1c8a57bc9647cd23c7631e
Mahedul-Karim <karimrupo21@gmail.com>
2026-02-09 16:41:50 +0600

    while loop and function

---
+++ day-6/main.py
@@ -0,0 +1,15 @@
+### function
+
+def first_function():
+    print("Hello World")
+
+first_function()
+
+
+### while loop
+
+number = 6
+
+while number >=0:
+    print(f"The number is {number}")
+    number -= 1
