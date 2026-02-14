7af2226cbcdaa00290574d59559ee29e110968bc
Mahedul-Karim <karimrupo21@gmail.com>
2026-02-14 16:34:25 +0600

    functions with parameter

---
+++ day-8/main.py
@@ -0,0 +1,7 @@
+### functions with parameters
+
+def sum_of_two_number(num1,num2):
+    print(f"Total sum of {num1} and {num2} is {num1+num2}")
+
+sum_of_two_number(2,3)
+sum_of_two_number(num1=6,num2=10)
