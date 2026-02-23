6aab2d148bff58c6d7cbe2d449a6a6ced790baf1
Mahedul-Karim <karimrupo21@gmail.com>
2026-02-23 16:43:21 +0600

    dictionaries

---
+++ day-9/main.py
@@ -0,0 +1,17 @@
+### dictionaries
+
+fruit_colors = {
+    "apple": "red",
+    "banana": "yellow"
+}
+
+
+fruit_colors["pear"] = "green"
+
+print(fruit_colors)
+
+print(fruit_colors["apple"])
+
+for key in fruit_colors:
+    print(key)
+    print(fruit_colors[key])
